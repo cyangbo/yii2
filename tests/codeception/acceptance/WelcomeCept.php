891cd0a0fa8bc8edb32be7fe1f33f382ab7205f5
cyangbo <yangbocai@YangboCai.snsshop.net>
2016-08-05 14:36:29 +0800

    配置域名,新增测试用例

--- tests/codeception/acceptance/WelcomeCept.php
+++ tests/codeception/acceptance/WelcomeCept.php
@@ -1,3 +1,9 @@
 <?php 
 $I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
+/**
+ * It will check that your frontpage contains the word Home in it
+ * 检查页面是否包含"Home"这个单词
+ */
+$I->wantTo('ensure that frontpage works');
+$I->amOnPage('/');
+$I->see('Home');
